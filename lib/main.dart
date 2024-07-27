b448f588d1b9b4343e93cd6a2949f5b20b24b05f
Elvis Barrionuevo <idcareg.g@gmail.com>
2024-07-26 20:23:59 -0500

    Mods

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,82 @@
-
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: HomePage(),
+    );
   }
 }
 
+class HomePage extends StatelessWidget {
+  List<String> names = [
+    "Daniel",
+    "Elvis",
+    "Cesar",
+    "Juan",
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Text(
+          "Contactos",
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                hintText: "Ingrese nombre completo",
+                label: Text("Alumno:"),
+                icon: Icon(Icons.people),
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  "Agregar",
+                ),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: names.length,
+                itemBuilder: (BuildContext context, int pepe) {
+                  return Text(names[pepe]);
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+
+      // body: ListView(
+      //   children: List.generate(100, (index)=> Text("Hola")),
+      // ),
+      // body: ListView.builder(
+      //   itemCount: names.length,
+      //   itemBuilder: (BuildContext context, int pepe){
+      //     return Text(names[pepe]);
+      //   },
+      // ),
+    );
+  }
+}
